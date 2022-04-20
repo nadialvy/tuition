ea77c79c535adb50a6ed191577ee4977cedf0a29
nadialvy <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2022-04-20 20:34:24 +0700

    Create Tuition

--- backend/app/Http/Controllers/TuitionController.php
+++ backend/app/Http/Controllers/TuitionController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Tuition;
 
 class TuitionController extends Controller
 {
-    //
+    // create data start
+    public function store(Request $req){
+        $validator = Validator::make( $req->all(), 
+        [
+            'generation' => 'required',
+            'year' => 'required',
+            'nominal' => 'required'
+        ]);
+
+        if($validator -> fails()){
+            return response()->json($validator->errors());
+        }
+
+        $store = Tuition::create([
+            'generation' => $req->generation,
+            'year' => $req->year,
+            'nominal' => $req->nominal
+        ]);
+        
+        $data = Tuition::where('generation', $req->generation)->first();
+        if($store){
+            return response()->json([
+                'status' => 'success',
+                'data' => $data
+            ]);
+        }else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to store data'
+            ]);
+        }
+    } 
+    // create data end 
 }
